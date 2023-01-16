198ba2419175ccf9da443a421826621c43ae7b36
singh-sukhpreet <sukhi.singh4424@gmail.com>
2023-01-16 15:20:16 +0530

    added new test cases

---
+++ cypress/e2e/tests/test7.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe('kitchen sink-7', () => {
+    const email = 'test@gmail.com'
+    const password = 'ABCabc123'
+    beforeEach(() => {
+      cy.visit('https://example.cypress.io/commands/querying')
+    })
+  
+    it('To find elements within a specific DOM element', () => {
+        cy.get('.query-form').within(() => {
+            cy.get('input:first').should('have.attr', 'placeholder', 'Email')
+            cy.get('input:first').type(email).should('have.value',email)
+          })
+
+        cy.get('.query-form').within(() => {
+          cy.get('input:last').should('have.attr', 'placeholder', 'Password')
+          cy.get('input:last').type(password).should('have.value',password)
+        })
+    });
+
+})
